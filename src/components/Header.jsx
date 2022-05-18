00e5bdb19ef62e8dc83ccfcf4e778f4e71ae5e6e
FherAmableRicse <amablericsefher@gmail.com>
2022-05-17 21:38:33 -0500

    update estilos index css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-const Header = () =>{
- return( <header>
-      <h1>
-        Control Presupuestario
-      </h1>
+const Header = ({company}) =>{
+  const {name, slogan} = company;
+ return( 
+ <header class="header">
+      <h2 className="header__name">{name}</h2>
+      <h1 className="header__slogan">{slogan}</h1>
   </header>
  )
 };
